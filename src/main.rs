01bf6e2d2386bdc38ebba002a3c0d0a48969c335
ethan <yinssh1005@gmail.com>
2024-09-16 23:36:07 +0800

    initial pg

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
-mod dbConnect;
+// mod dbConnect;
 
-use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
-use chrono_tz::Tz;
-use chrono::*;
 use clap::{Arg, Command};
-use dialoguer::{Input, Confirm};
-
-fn tz_convert(datestr: &str, target_tz: &str) -> Option<DateTime<Tz>> {
-    let naive_datetime = NaiveDateTime::parse_from_str(datestr, "%Y-%m-%d %H:%M:%S").ok()?;
-    let tz: Tz = target_tz.parse().ok()?;
-
-    // Some(tz.from_local_datetime(&naive_datetime).single()?)
-    Some(tz.from_local_datetime(&naive_datetime).single()?)
-}
-
-// This function takes time in UTC and returns time in target Timezone
-fn convert_to_timezone(dt: DateTime<Utc>, target_tz: Tz) -> DateTime<Tz> {
-    // Here we convert the DateTime<Utc> to DateTime<FixedOffset>
-    let dtf = dt.with_timezone(&target_tz);
-
-    dtf
-}
+use dialoguer::Input;
 
 // Example usage:
 
@@ -30,58 +11,30 @@ fn main() {
         .author("Ethan.Yin <ethan.yin@jetbrains.com>")
         .about("A simple interactive CLI tool example")
         .arg(
-            Arg::new("name")
-                .short('n')
-                .long("name")
-                // .value_name("NAME")
-                // .help("-h")
-                .takes_value(true)
+            Arg::new("option")
+            .help("-h")
         )
         .get_matches();
 
-    let name: String = match matches.value_of("name") {
-        Some(name) => name.to_string(),
-        None => Input::new()
-            .with_prompt("What's your name?")
-            .interact_text()
-            .unwrap(),
-    };
-
-    let age: u32 = Input::new()
-        .with_prompt("How old are you?")
+    let meetingDateTime: String = Input::new()
+        .with_prompt("Please type the meeting time in your timezone 'YYYY-MM-DD HH:MM' format.")
         .interact_text()
         .unwrap();
 
-    let likes_rust: bool = Confirm::new()
-        .with_prompt("Do you like Rust?")
-        .default(true)
-        .interact()
+    let localTimezone: String = Input::new()
+        .with_prompt("Your timezone? (UTC+8)")
+        .default("UTC+8".to_string())
+        .interact_text()
         .unwrap();
 
-    println!("\nSummary:");
-    println!("Name: {}", name);
-    println!("Age: {}", age);
-    println!("Likes Rust: {}", if likes_rust { "Yes" } else { "No" });
+    let targetTimezone: String = Input::new()
+        .with_prompt("Target timezone?(UTC+2)")
+        .default("UTC+2".to_string())
+        .interact_text()
+        .unwrap();
 
-    // println!("1st, convert the fixed timezone string to the target timezone format");
-    // println!("...");
-    //
-    // match tz_convert("2024-01-01 01:00:00", "Europe/Berlin") {
-    //     Some(dt) => println!("{}", dt),
-    //     None => println!("Invalid date/time or timezone"),
-    // }
-    // println!("2nd, calculate the fixed timezone string to the target timezone");
-    // println!("...");
-    //
-    // // let utc_dt = Utc.with_ymd_and_hms(2024, 1, 1, 0, 30, 0);
-    // // Create a datetime in UTC
-    // let utc_dt = Utc.ymd(2024, 1, 1).and_hms(0, 30, 0);
-    //
-    // // Set up the target timezone
-    // let jst = chrono_tz::Asia::Tokyo;
-    //
-    // // Here we convert the time to Tokyo's timezone
-    // let result = convert_to_timezone(utc_dt, jst);
-    //
-    // println!("Result: {}", result);
+    println!("\nThe meeting time is going to be:");
+    println!("Date & Time: {}", meetingDateTime);
+    println!("Your Timezone: {}", localTimezone);
+    println!("Target Timezone: {}", targetTimezone);
 }
